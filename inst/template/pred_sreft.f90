b1a923b158ff4d0186a94c3e956308f1dd681c0b
Ryota Jin <70737858+RyotaJin@users.noreply.github.com>
2025-08-20 21:54:34 +0900

    fix typo

--- inst/template/pred_sreft.f90
+++ inst/template/pred_sreft.f90
@@ -169,20 +169,21 @@
 
       allocate (a(numbm), b(numbm), c(numbm), meanx(numbm), meany(numbm), coun(numbm), covy(numbm))
 
-      if (newind<=1) call initialize(id, meanx, meany, coun, a, b, c, bm, serial, covt, covy)
-
       id = datrec({id_num})
       serial = datrec({serial_num})
       time  = datrec({time_num})
-      bm = daterec({bm_num})
+      bm = datrec({bm_num})
       meanx = datrec((/{meanx_num}/))
       meany = datrec((/{meany_num}/))
       coun = datrec((/{coun_num}/))
+      
       a = theta(1:numbm) + eta(1:numbm)
       b = theta(1+numbm:2*numbm) + eta(1+numbm:2*numbm)
       c = theta(1+2*numbm:3*numbm) + eta(1+2*numbm:3*numbm)
       covt = 1.0d0{covt_code}
       covy = 0.0d0{covy_code}
+
+      if (newind<=1) call initialize(id, meanx, meany, coun, a, b, c, bm, serial, covt, covy)
 
       call eval(a, b, c, f, g, time, bm, serial, covt, covy)
 
